204cb9644a6daefe3a1360ed61b875a5476aca98
abotlp <patriciamirelabota@gmail.com>
2026-06-27 14:13:22 +0200

    Add TF model text cleanup

---
+++ static/tf_model_clean.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.summary-note, .model-subnote, .summary-table').forEach(function (el) { el.style.display = 'none'; });
+});
